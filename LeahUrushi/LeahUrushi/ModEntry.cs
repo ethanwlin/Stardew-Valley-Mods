9d683ab30474f165b4937441440e0e634ef6c097
Mohong <ethanwlin@gmail.com>
2021-01-18 19:59:52 -0800

    Leah Urushi Mod
    Replaces Leah protrait with Urushi

---
+++ LeahUrushi/LeahUrushi/ModEntry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Objects;
+
+namespace DoubleChest
+{
+    public class ModEntry : Mod, IAssetLoader
+    {
+        public bool CanLoad<T>(IAssetInfo asset)
+        {
+            if (asset.AssetNameEquals("Portraits/Leah"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>Load a matched asset.</summary>
+        /// <param name="asset">Basic metadata about the asset being loaded.</param>
+        public T Load<T>(IAssetInfo asset)
+        {
+            if (asset.AssetNameEquals("Portraits/Leah"))
+            {
+                return this.Helper.Content.Load<T>("assets/urushi.png", ContentSource.ModFolder);
+            }
+
+            throw new InvalidOperationException($"Unexpected asset '{asset.AssetName}'.");
+            throw new NotImplementedException();
+        }
+
+        public override void Entry(IModHelper helper)
+        {
+            return;
+        }
+    }
+}
